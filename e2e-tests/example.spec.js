4514bca17c6f236245f505064254df3db2ad9800
Danny McKinnon <dannymckinnon.dm@gmail.com>
2024-04-20 15:36:59 -0700

    Add test to navigate to specific pokemon page

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -6,4 +6,11 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('/')
+    const link = await page.getByText('ivysaur')
+    link.click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
